b0578a65cdbdc0f0e1c0f4e6ede4776d0f9fe0fe
Michael Rybka <merybka@gmail.com>
2023-10-05 16:15:34 -0400

    Add ReadJSONFile()

---
+++ read_json_file.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func ReadJSONFile(path string, v any) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewDecoder(f).Decode(v)
+}
