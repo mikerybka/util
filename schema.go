b6723fd686b3b20fc3847f31c5ebcfb7d8ed673a
Mike Rybka <merybka@gmail.com>
2024-05-28 07:02:44 -0400

    wip

--- schema.go
+++ schema.go
@@ -11,6 +11,13 @@ type Schema struct {
 	Fields []Field
 }
 
+func (s *Schema) Type() *Type {
+	return &Type{
+		IsStruct: true,
+		Fields:   s.Fields,
+	}
+}
+
 func (s *Schema) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		json.NewEncoder(w).Encode(s)
