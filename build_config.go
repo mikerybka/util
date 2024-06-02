14b828374051dfc8cd1fa4237734c634ce9c219a
Mike Rybka <merybka@gmail.com>
2024-06-02 14:14:02 -0400

    wip

--- build_config.go
+++ build_config.go
@@ -1,13 +1,25 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 type BuildConfig struct {
 	Type string
 }
 
-func (config *BuildConfig) Build(path string) error {
+func (config *BuildConfig) Build(path string, out string) error {
 	switch config.Type {
+	case "go":
+		cmd := exec.Command("go", "build", "-o", out, ".")
+		cmd.Dir = path
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(string(out))
+			return err
+		}
+		return nil
 	default:
 		panic(fmt.Sprintf("unknown type: %s", config.Type))
 	}
