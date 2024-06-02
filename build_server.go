2209a0c8ae67593d8adea1e3e8e2b1d74f7fcabc
Mike Rybka <merybka@gmail.com>
2024-06-02 13:51:27 -0400

    wip

--- build_server.go
+++ build_server.go
@@ -1,5 +1,19 @@
 package util
 
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
 type BuildServer struct {
 	Workdir string
+}
+
+func (s *BuildServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	b, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
 }
