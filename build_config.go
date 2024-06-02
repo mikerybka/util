7598b6a8aaefb3b1fe0013986cba99b189f91db6
Mike Rybka <merybka@gmail.com>
2024-06-02 14:23:42 -0400

    wip

--- build_config.go
+++ build_config.go
@@ -6,21 +6,34 @@ import (
 )
 
 type BuildConfig struct {
-	Type string
+	Type      string
+	Path      string
+	Out       string
+	OnSuccess string
 }
 
-func (config *BuildConfig) Build(path string, out string) error {
+func (config *BuildConfig) Build() error {
 	switch config.Type {
 	case "go":
-		cmd := exec.Command("go", "build", "-o", out, ".")
-		cmd.Dir = path
+		cmd := exec.Command("go", "build", "-o", config.Out, ".")
+		cmd.Dir = config.Path
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println(string(out))
 			return err
 		}
-		return nil
 	default:
 		panic(fmt.Sprintf("unknown type: %s", config.Type))
 	}
+
+	// Run post-build script
+	cmd := exec.Command("bash", "-c", config.OnSuccess)
+	cmd.Dir = config.Path
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		return err
+	}
+
+	return nil
 }
