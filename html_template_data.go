7b5cb65b7c094872d20ef15001ae77c21c1eb06c
Mike Rybka <merybka@gmail.com>
2024-06-02 11:47:18 -0400

    wip

---
+++ html_template_data.go
@@ -0,0 +1,8 @@
+package util
+
+type HTMLTemplateData struct {
+	Title    string
+	Desc     string
+	Author   string
+	Keywords string
+}
--- web_frontend.go
+++ web_frontend.go
@@ -1,12 +1,55 @@
 package util
 
-import "net/http"
+import (
+	_ "embed"
+	"html/template"
+	"net/http"
+)
 
+//go:embed web/dist/main.js
 var mainJS []byte
-var mainHTML []byte
+
+//go:embed web/dist/main.css
+var mainCSS []byte
+
+var htmlTemplate string = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>{{ .Title }}</title>
+		<meta name="description" content="{{ .Desc }}"/>
+		<meta name="author" content="{{ .Author }}" />
+		<meta name="keywords" content="{{ .Keywords }}" />
+		<link rel="stylesheet" href="/main.css" type="text/css" />
+  	</head>
+	<body>
+		<main id="root">Loading...</main>
+		<script src="/main.js"></script>
+	</body>
+</html>`
 
 type WebFrontend struct {
-	Favicon []byte
+	Favicon      []byte
+	RootTitle    string
+	MetaDesc     string
+	MetaAuthor   string
+	MetaKeywords []string
+}
+
+func (fe *WebFrontend) HTMLData() *HTMLTemplateData {
+	keywords := ""
+	for i, kw := range fe.MetaKeywords {
+		if i > 0 {
+			keywords += ","
+		}
+		keywords += kw
+	}
+	return &HTMLTemplateData{
+		Title:    fe.RootTitle,
+		Desc:     fe.MetaDesc,
+		Author:   fe.MetaAuthor,
+		Keywords: keywords,
+	}
 }
 
 func (fe *WebFrontend) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -18,5 +61,13 @@ func (fe *WebFrontend) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Write(mainJS)
 		return
 	}
-	w.Write(mainHTML)
+	if r.URL.Path == "/main.css" {
+		w.Write(mainCSS)
+		return
+	}
+
+	err := template.Must(template.New("main.html").Parse(htmlTemplate)).Execute(w, fe.HTMLData())
+	if err != nil {
+		panic(err)
+	}
 }
