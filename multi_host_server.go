e60d0cd4f0c82bf92ce9ed65d6164daebd186328
Mike Rybka <merybka@gmail.com>
2024-06-09 18:38:12 -0400

    wip

--- multi_host_server.go
+++ multi_host_server.go
@@ -3,13 +3,18 @@ package util
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"runtime/debug"
 
 	"golang.org/x/crypto/acme/autocert"
 )
 
 type MultiHostServer struct {
-	Hosts map[string]http.Handler
+	Hosts        map[string]http.Handler
+	TwilioClient *TwilioClient
+	AdminPhone   string
 }
 
 func (s *MultiHostServer) HostPolicy(ctx context.Context, host string) error {
@@ -21,12 +26,31 @@ func (s *MultiHostServer) HostPolicy(ctx context.Context, host string) error {
 }
 
 func (s *MultiHostServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Handle panics by notifying the admin.
+	defer func() {
+		if err := recover(); err != nil {
+			// Log the error and stack trace
+			log.Printf("Panic recovered: %v\n%s", err, debug.Stack())
+			// Respond with a 500 Internal Server Error
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	}()
+
+	// Log the request.
+	b, _ := httputil.DumpRequest(r, true)
+	log.Println(string(b))
+
+	// Find the handler for the host.
 	h, ok := s.Hosts[r.Host]
 	if !ok {
 		http.NotFound(w, r)
 		return
 	}
+
+	// Turn off CORS.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Server the request.
 	h.ServeHTTP(w, r)
 }
 
