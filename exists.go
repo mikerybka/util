b86428c590ca9e8b0100365526d3de111e64c5bd
Mike Rybka <merybka@gmail.com>
2025-04-02 14:34:09 -0400

    Fix bug in util.Exists

--- exists.go
+++ exists.go
@@ -7,10 +7,10 @@ import (
 
 func Exists(path string) bool {
 	_, err := os.Stat(path)
-	if errors.Is(err, os.ErrNotExist) {
-		return false
-	}
 	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false
+		}
 		panic(err)
 	}
 	return true
