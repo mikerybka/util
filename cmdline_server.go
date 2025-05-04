fd2c96c77b37580a4c3e3ffba49b46a87584a7f3
Mike Rybka <merybka@gmail.com>
2025-05-04 11:34:26 -0400

    Add filepath to cmdline server

--- cmdline_server.go
+++ cmdline_server.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,15 +10,18 @@ import (
 	"strings"
 )
 
-// CmdlineServer let's you easily turn any http.Handler into a cmdline app.
+// CmdlineServer let's you easily turn any http.Handler into a cmdline app that operates on local data.
 func CmdlineServer(h http.Handler) {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s <method> <url> [header1=val1,...]\nStdin is the request body.\n", os.Args[0])
+	if len(os.Args) < 4 {
+		fmt.Printf("Usage: %s <filepath> <method> <url> [header1=val1,...]\nStdin is the request body.\n", os.Args[0])
 		return
 	}
-	method := os.Args[1]
-	url := os.Args[2]
-	headers := os.Args[3:]
+	path := os.Args[1]
+	b, _ := os.ReadFile(path)
+	json.Unmarshal(b, h)
+	method := os.Args[2]
+	url := os.Args[3]
+	headers := os.Args[4:]
 	r, err := http.NewRequest(method, url, os.Stdin)
 	if err != nil {
 		panic(err)
@@ -31,6 +35,12 @@ func CmdlineServer(h http.Handler) {
 	}
 	w := httptest.NewRecorder()
 	h.ServeHTTP(w, r)
+	if IsMutation(r) {
+		err = WriteJSONFile(path, h)
+		if err != nil {
+			panic(err)
+		}
+	}
 	res := w.Result()
 	fmt.Println(res.StatusCode)
 	_, err = io.Copy(os.Stdout, res.Body)
